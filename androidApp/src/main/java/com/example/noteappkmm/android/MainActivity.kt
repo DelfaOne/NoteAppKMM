3b5267fd66b805563ab9e59249ceebd01b7cc0be
Fadel <fadel.foudi@gmail.com>
2024-02-14 16:44:13 -0500

    Feat: Add components for NoteListScreen and DI

--- androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
@@ -3,24 +3,20 @@ package com.example.noteappkmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteappkmm.android.notelist.ui.NoteListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //GreetingView()
-                }
+               NoteListScreen()
             }
         }
     }
